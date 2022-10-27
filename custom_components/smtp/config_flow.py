fe78745b6c359574dfd8d804290339be018abedf
shaonianzhentan <ilovevar@gmail.com>
2022-10-27 18:34:29 +0800

    显示基本信息

--- custom_components/smtp/config_flow.py
+++ custom_components/smtp/config_flow.py
@@ -2,12 +2,17 @@ from __future__ import annotations
 
 from typing import Any
 import voluptuous as vol
+import time
 
+from homeassistant.const import __version__ as current_version
 from homeassistant.config_entries import ConfigFlow
 from homeassistant.data_entry_flow import FlowResult
+from homeassistant.helpers.network import get_url
+from .manifest import manifest
 
 from .const import DOMAIN
 from .qqmail import QQMail
+
 
 DATA_SCHEMA = vol.Schema({
     vol.Required("qq"): str,
@@ -31,8 +36,15 @@ class SimpleConfigFlow(ConfigFlow, domain=DOMAIN):
             code = user_input['code']
             # 安装校验
             qm = QQMail(self.hass, qq, code)
-            result = qm.send('HomeAssistant插件安装成功', '''
-            插件地址：https://github.com/shaonianzhentan/smtp
+
+            base_url = get_url(self.hass)
+            today = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+
+            result = qm.send('HomeAssistant插件安装成功', f'<br />安装时间：{today}' 
+                + f'<br />服务地址：{base_url}'
+                + f'<br />系统版本：{current_version}'
+                + f'<br />插件版本：{manifest.version}' + '''
+            <br />插件地址：https://github.com/shaonianzhentan/smtp
 
             关注微信公众号，了解更多HomeAssistant信息
             <br/><img src="https://ha.jiluxinqing.com/img/wechat-channel.png" height="160" alt="HomeAssistant家庭助理" title="HomeAssistant家庭助理">
